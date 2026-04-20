96ecb10ad0ccd04deeed40423ae110b16dc6ee2d
Benjamin Emmanuel <benjaminemmanuel2017@gmail.com>
2026-04-20 18:10:33 +0100

    Updated Controller Implementation

--- src/main/java/com/employeemanager/crudtask/controller/EmployeeController.java
+++ src/main/java/com/employeemanager/crudtask/controller/EmployeeController.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/employees")
+@RequestMapping("/api/v1/employees") //The base url
 
 public class EmployeeController {
     private final EmployeeService employeeService;
 
+    //Constructor of the Employee Service
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
@@ -19,5 +20,11 @@ public class EmployeeController {
     @PostMapping
     public Employee save(@RequestBody Employee employee) {
         return employeeService.create(employee);
+    }
+
+    //Updating an employee
+    @PutMapping
+    public Employee update(@RequestBody Employee employee) {
+        return employeeService.update(employee);
     }
 }
