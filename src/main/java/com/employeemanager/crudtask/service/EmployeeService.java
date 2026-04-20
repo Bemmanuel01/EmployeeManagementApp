d60fa25d92b91ac80aca411ae3b2e35c632bd26d
Benjamin Emmanuel <benjaminemmanuel2017@gmail.com>
2026-04-20 17:51:04 +0100

    Employee Service Implementation

--- src/main/java/com/employeemanager/crudtask/service/EmployeeService.java
+++ src/main/java/com/employeemanager/crudtask/service/EmployeeService.java
@@ -1,5 +1,14 @@
 package com.employeemanager.crudtask.service;
 
-public interface EmployeeService{
+import com.employeemanager.crudtask.entity.Employee;
+import java.util.*;
 
+//The services available on the employee manager's app
+public interface EmployeeService{
+    Employee create(Employee employee);
+    Employee update(Employee employee);
+    Employee getById(Long id);
+
+    List<Employee> getAll();
+    void delete(Long id);
 }
---
+++ src/main/java/com/employeemanager/crudtask/service/EmployeeServiceImplementation.java
@@ -0,0 +1,44 @@
+package com.employeemanager.crudtask.service;
+
+import com.employeemanager.crudtask.entity.Employee;
+import com.employeemanager.crudtask.repository.EmployeeRepository;
+import com.employeemanager.crudtask.service.EmployeeService;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class EmployeeServiceImplementation implements EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImplementation(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Employee create(Employee employee) {
+        return null;
+    }
+
+    @Override
+    public Employee update(Employee employee) {
+        return null;
+    }
+
+    @Override
+    public Employee getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Employee> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
+
+
