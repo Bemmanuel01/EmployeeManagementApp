d9498ed12881e91aac465ea5ae55e7afcc61c664
Benjamin Emmanuel <benjaminemmanuel2017@gmail.com>
2026-04-20 17:21:35 +0100

    Employee Entity implementation

--- src/main/java/com/employeemanager/crudtask/entity/Employee.java
+++ src/main/java/com/employeemanager/crudtask/entity/Employee.java
@@ -1,4 +1,93 @@
 package com.employeemanager.crudtask.entity;
 
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "employee")
+
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true) //Unique email for each employee
+    private String email;
+
+    private String department;
+    private BigDecimal salary;
+
+    private LocalDate dateOfJoining;
+    private Boolean active;
+
+    //Getters & Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getDateOfJoining() {
+        return dateOfJoining;
+    }
+
+    public void setDateOfJoining(LocalDate dateOfJoining) {
+        this.dateOfJoining = dateOfJoining;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 }
